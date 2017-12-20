a5d06ddb58695ea94df9b0adc96a2c832f5408c8
tangchaoren1 <735405280@qq.com>
2017-12-20 15:05:53 +0800

    audit app.vue

--- app.vue
+++ app.vue
@@ -1,40 +1,38 @@
 <template>
   <div class="daily">
-      <div class="daily-menu">
-          <ul>
-              <li v-for="(menu,index) in menus" class="deily-menu-item"  
-              @click="channelRecommand(index)">
-                  <a href="#" :class="{active: foundMenuIndex == index}">{{menu.item}}</a>
-              </li>
-          </ul>
-      </div>
+     <div class="daily-menu-item" :class="{on: type === 'recommend'}" 
+     @click="handleToRecommend">
+        每日推荐
+     </div>
+     <div class="daily-menu-item" :class="{on: type === 'daily'}"
+      @click="showThems = !showThems">
+         主题日报
+     </div>
+     <ul v-show="showThems">
+         <li v-for="item in thems">
+             <a :class="{on: item.id === themeId && type === 'daily'}" 
+             @click="handleToTheme(item.id)">{{item.name}}
+             </a>
+         </li>
+     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      foundMenuIndex:0,  
-      menus: [
-        { item: "日常心理学" },
-        { item: "用户推荐日报"},
-        { item: "电影日报"},
-        { item: "不许无聊"},
-        { item: "设计日报"},
-        { item: "大公司日报"},
-        { item: "财经日报"},
-        { item: "互联网安全"},
-        { item: "游戏"},
-        { item: "更多"}
-      ]
-    };
+  data(){
+      return {
+          thems:[],
+          showThems: false,
+      }
   },
-  computed: {},
   methods: {
-    channelRecommand: function(i) {
-      this.foundMenuIndex = i;
-    }
+      handleToRecommend(){
+          this.type = 'recommand';
+          this.recommendList = [];
+
+      }
   }
-};
+}
 </script>
+
